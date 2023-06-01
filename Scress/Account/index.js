539f46d1057aee0ee06dafc6aa815c7154ae92f0
Hionomo <phuclhph20309@fpt.edu.vn>
2023-06-01 14:22:12 +0700

    phuc chinh sua man hinh account

--- Scress/Account/index.js
+++ Scress/Account/index.js
@@ -3,17 +3,20 @@ import { View, Text, StyleSheet, Image } from "react-native";
 import { Pressable } from "react-native";
 const Account = (props) => {
   const navigation = props.navigation;
-  const ondmk =() => {
+  const ondmk = () => {
     navigation.navigate('doimk');
   }
   return (
     <View style={styles.container}>
       <View style={styles.heard}>
         <Image style={styles.img} source={require("../../Avata/avata.png")} />
-        <Text style={styles.text}>hoangtv@gmail.com</Text>
+        <View>
+          <Text style={styles.title}>Tài khoản của bạn</Text>
+          <Text style={styles.text}>hoangtv@gmail.com</Text>
+        </View>
       </View>
 
-      <Pressable style={styles.button} onPress={ () => ondmk()}>
+      <Pressable style={styles.button} onPress={() => ondmk()}>
         <Text style={styles.textButton}>Đổi mật khẩu</Text>
       </Pressable>
 
@@ -33,6 +36,10 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 186,
     backgroundColor: "#62CDFF",
+    flexDirection: 'row'
+  },
+  title: {
+    fontSize: 20, width: 184, height: 33, marginTop: 24
   },
   img: {
     height: 104,
@@ -41,22 +48,29 @@ const styles = StyleSheet.create({
     marginLeft: 31,
   },
   text: {
+    width: 150,
+    height: 33,
+    fontWeight: "400",
     fontSize: 16,
-    marginLeft: 31,
-    marginTop: 10,
+    marginLeft: 21,
+    marginTop: 30,
   },
   button: {
     width: "95%",
     height: 63,
     backgroundColor: "#62CDFF",
-    marginTop: 35,
+    marginTop: 33,
     marginLeft: 10,
-    borderRadius: 10,
+    borderRadius: 20,
   },
   textButton: {
     fontSize: 20,
     textAlign: "center",
+    alignSelf: "center",
     lineHeight: 63,
+    width: "100%",
+    height: 63,
+    fontWeight: "500",
   },
 });
 
